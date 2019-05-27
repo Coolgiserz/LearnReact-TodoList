0878392d1bb73a181679140d2bc509114b5eb46f
Coolgiserz <0106160226@csu.edu.cn>
2019-05-27 19:54:33 +0800

    CSS过渡效果

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 //all in js
 // import './index.css';
 import Todolist from './Todolist';//ES6的模块引用语法，从当前目录引用App，脚手架工具会自动补全js后缀，载入App.js
-
-ReactDOM.render(<Todolist />, document.getElementById('root'));
+import App from './App';
+// ReactDOM.render(<Todolist />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
