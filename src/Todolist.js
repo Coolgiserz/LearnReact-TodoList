7efb2efd1e2500133d51612822d584977864216c
Coolgiserz <0106160226@csu.edu.cn>
2019-05-27 21:25:26 +0800

    antd usage

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,9 +1,21 @@
 import React, {Component, Fragment} from 'react';//Fragment:占位符
-import './style.css';
-import TodoItem from './TodoItem';
+import 'antd/dist/antd.css'
+import {Input, Button, List} from 'antd';
+
+/**
+ * antd 中 Input、Button、List等组件的基本使用
+ * https://ant.design/components/list-cn/
+ * @type {string[]}
+ */
+const data = [
+    'CSU',
+    'HNU',
+    'QNU',
+];
 class Todolist extends React.Component{//JSX必须在最外层包一个元素
 	//使用Todolist时，构造函数优先执行
-	constructor(props){ //固定写法
+
+    constructor(props){ //固定写法
 		super(props);
 		this.state = {//组件状态
 			inputvalue: '生活艰难',//输入框中的内容，要将其与输入框绑定
@@ -21,18 +33,21 @@ class Todolist extends React.Component{//JSX必须在最外层包一个元素
 		const inputvalue = this.state.inputvalue;
 		return (
 			<Fragment>
-                <label htmlFor="insertArea">输入内容</label>
-
-				<input
-					id="insertArea"
-					className='input'
-					value={inputvalue}
-					onChange={this.handleInputChange}
+				<div>
+				<Input
+					placeholder='To do'
+					style={{width : '300px' , marginLeft:'10px'}}
 				/>
-				<button onClick={this.handleButtonClick}>提交</button>
-				<ul>
-					{this.getToDoItem()}
-				</ul>
+					<Button type='primary'>提交</Button>
+				</div>
+                <List
+                    size="small"
+                    header={<div>Header</div>}
+                    footer={<div>Footer</div>}
+                    bordered
+                    dataSource={data}
+                    renderItem={item => <List.Item>{item}</List.Item>}
+                />
 			 </Fragment>
 			)
 	}
@@ -41,47 +56,16 @@ class Todolist extends React.Component{//JSX必须在最外层包一个元素
 
 	}
     handleItemDelete(index){//点击后将数据从数组删除
-		const list = [...this.state.list];//...是展开运算符，此操作拷贝了一份数据
-		list.splice(index,1);//删除下标为index的list，splice
-		console.log(index);
-		this.setState({
-			list
-		});
+
 	}
     handleButtonClick(e){
-		this.setState((prevState)=>({
-			list: [...prevState.list,prevState.inputvalue],
-			inputvalue:''
-		}));
+
 	}
 
 	getToDoItem(){
-        return (this.state.list.map((item,index)=>{
-            return(
-                <li key = {index}>
-                    <TodoItem
 
-                        content={item}
-                        index={index}
-                        deleteItem={this.handleItemDelete}/>
-                </li>
-            )
-
-        }))
 	}
 	handleInputChange(e){
-		console.log(this);
-		//旧写法
-		// this.setState({
-		// 	inputvalue: e.target.value
-		// });
-		//新写法
-		const value = e.target.value;
-        this.setState(()=>({
-				inputvalue: value
-        }));
-		//不对的写法this.state.inputvalue = e.target.value;//这种写法不对！此时this为undefine而不是Todolist的组件
-		console.log(e.target.value);
 
 	}
 }
